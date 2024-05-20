d664edafe6616df20a7d1670359383293fea150e
nguyenlamnhat <zznhatphilong@gmail.com>
2024-05-20 16:00:31 +0700

    done config router

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
+import React from "react";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Search from "./components/users/Search";
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <Navbar />
-      <div className="container">
-        <h1>Hello from React</h1>
-      </div>
-      <Search />
+      <Router>
+        <Navbar />
+        <div className="container">
+          <h1>GitHub Users Data</h1>
+          <Switch>
+            <Route exact path="/" component={Search} />
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
-}
+};
 export default App;
 
