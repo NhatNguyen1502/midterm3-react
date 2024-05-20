5fd570d5b9261aad332b86dcf6e43ef9355f71b6
nguyenlamnhat <zznhatphilong@gmail.com>
2024-05-20 15:54:11 +0700

    done feat search user

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,16 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
-import Users from "./components/users/User";
+import Search from "./components/users/Search";
 function App() {
-  const [users, setUsers] = useState([]);
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get("https://api.github.com/users");
-        setUsers(response.data);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-    fetchData();
-  }, []);
   return (
     <div className="App">
       <Navbar />
       <div className="container">
         <h1>Hello from React</h1>
       </div>
-      <Users users={users} />
+      <Search />
     </div>
   );
 }
 export default App;
-
 
